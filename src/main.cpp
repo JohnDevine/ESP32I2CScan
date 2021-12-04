3fd70d1fb9775dc05b80aece6b74f32ad8a9883f
John Devine <you@example.comJohn.H.Devine@gmail.com>
2021-12-04 15:47:27 +0700

    Added ability to change pins & docos

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,58 @@
 #include <Arduino.h>
 
 #include <Wire.h>
- 
-void setup() {
-  Wire.begin();
+
+void setup()
+{
+  // When using the ESP32 with the Arduino IDE, use the Wire.h library to communicate with devices using I2C.
+  // With this library, you initialize the I2C as follows:
+  // Wire.begin(I2C_SDA, I2C_SCL);
+  // viable options for SDA are 21, 33
+  // viable options for SCL are 22, 32
+  // Default if there are no arguments are SDA=21, SCL=22
+  
+  Wire.begin(21, 22);
   Serial.begin(115200);
   Serial.println("\nI2C Scanner");
 }
- 
-void loop() {
+
+void loop()
+{
   byte error, address;
   int nDevices;
   Serial.println("Scanning...");
   nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
+  for (address = 1; address < 127; address++)
+  {
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
-    if (error == 0) {
+    if (error == 0)
+    {
       Serial.print("I2C device found at address 0x");
-      if (address<16) {
+      if (address < 16)
+      {
         Serial.print("0");
       }
-      Serial.println(address,HEX);
+      Serial.println(address, HEX);
       nDevices++;
     }
-    else if (error==4) {
+    else if (error == 4)
+    {
       Serial.print("Unknow error at address 0x");
-      if (address<16) {
+      if (address < 16)
+      {
         Serial.print("0");
       }
-      Serial.println(address,HEX);
-    }    
+      Serial.println(address, HEX);
+    }
   }
-  if (nDevices == 0) {
+  if (nDevices == 0)
+  {
     Serial.println("No I2C devices found\n");
   }
-  else {
+  else
+  {
     Serial.println("done\n");
   }
-  delay(5000);          
+  delay(5000);
 }
